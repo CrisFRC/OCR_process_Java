011ff4dcd3c598de45a606ca33914988a41dc110
Cristian Roa <cfroa@bancolombia.com.co>
2023-03-21 17:23:56 -0500

    opencv dependency

--- src/main/java/utils/Data_capture.java
+++ src/main/java/utils/Data_capture.java
@@ -2,6 +2,10 @@ package utils;
 
 public class Data_capture {
 
+    public static String[] getWordFromDB(String text){
+        String clean = text.replaceAll(" ","");
+        String[] textWished = clean.split(";");
+        return textWished;
+    }
 
-    
 }
--- src/main/java/utils/Segmentation.java
+++ src/main/java/utils/Segmentation.java
@@ -1,4 +1,15 @@
 package utils;
 
+import org.opencv.core.*;
+import org.opencv.imgcodecs.Imgcodecs;
+import org.opencv.imgproc.Imgproc;
+
+
+
 public class Segmentation {
+
+    public static void thresholdSegmentation(String path) {
+        Mat imagen
+    }
+
 }
